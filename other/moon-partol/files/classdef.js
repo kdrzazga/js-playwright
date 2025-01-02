0a69de61e503e1e01d42890ada6ed5d5acf893a9
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-01-02 17:55:33 +0100

    moon patrol 2 + phaser tiling example

--- other/moon-partol/files/classdef.js
+++ other/moon-partol/files/classdef.js
@@ -1,21 +1,17 @@
 class Constants{
-    static SCREEN_WIDTH = 800;
+    static SCREEN_WIDTH = 1200;
     static SCREEN_HEIGHT = 600;
 }
 
 class Ground {
     constructor(scene) {
         this.scene = scene;
-        this.sprite = this.scene.add.tileSprite(400, 550, 800, 100, 'ground');
-        this.speed = 1;
+        this.sprite = this.scene.add.tileSprite(0, 550, 2600, 100, 'ground');
+        this.speed = 4;
     }
 
     update() {
-        this.sprite.x -= this.speed;
-
-        if (this.sprite.x <= -16300 + 800) {
-            this.sprite.x = 163000;
-        }
+        this.sprite.tilePositionX += this.speed;
     }
 }
 
