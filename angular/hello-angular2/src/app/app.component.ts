fc83484e73bfd77872e451576b50a648c4cbec71
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-02-09 11:53:59 +0100

    Angular 1 (#104)

---
+++ angular/hello-angular2/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'hello angular 2';
+  selectedRadio: string = 'radio 1';
+
+  onOkClick(){
+    window.alert('OKAY');
+  }
+
+  onCheckboxChange(optionValue: string, isChecked: boolean) {
+    console.log(optionValue);
+  }
+
+  onRadioClick(value: string){
+    console.log(`Radio ${value}.`);
+  }
+}
