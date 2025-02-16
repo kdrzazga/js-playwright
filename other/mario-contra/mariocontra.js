f7ce99a39942ee16cb7db1229dbb40fe80ce7e67
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-02-16 20:26:46 +0100

    MarioContra 2

--- other/mario-contra/mariocontra.js
+++ other/mario-contra/mariocontra.js
@@ -1,28 +1,57 @@
-class Constants{
-    SCREEN_WIDTH = 1200;
-    SCREEN_HEIGHT = 800;
-}
 
-class LevelScene extends Phaser.Scene {
-    constructor(levelName) {
-        super({ key: levelName });
+
+class MainScene extends Phaser.Scene {
+    constructor() {
+        super('MainScene');
     }
+
+ preload() {
+  // Load a simple sprite sheet or image for the sprite
+  this.load.image('ground', 'temp/sprite.png');
+  this.load.image('commando', 'temp/commando.png');
 }
 
-const levelObject = new LevelScene('Level 1');
+// Create game objects
+ create() {
+  this.cursors = this.input.keyboard.createCursorKeys();
 
+  // Create a group to hold all the sprites
+  this.spriteGroup = this.add.group();
+
+  // Loop through 20 iterations
+  for (let i = 0; i < 200; i++) {
+    // Calculate the x position based on the iteration number
+    const x = i * 60;
+
+    // Create a new sprite at the calculated x position and bottom of the screen
+    const sprite = this.add.sprite(x, this.sys.canvas.height - 50, 'ground');
+
+    // Add the sprite to the group
+    this.spriteGroup.add(sprite);
+
+    this.commando = this.add.sprite(70, this.sys.canvas.height - 150, 'commando');
+  }
+}
+
+// Update game logic
+ update(time, delta) {
+  // Check if the right arrow key is pressed
+  if (this.cursors.right.isDown) {
+    // Move all sprites in the group to the left
+    this.spriteGroup.children.iterate(function (child) {
+      child.x -= 5;
+    });
+  }
+}
+}
+
+// Create a Phaser game instance
 const config = {
-    type: Phaser.AUTO,
-    width: Constants.SCREEN_WIDTH,
-    height: Constants.SCREEN_HEIGHT,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: [levelObject]
-};
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 800,
+  height: 600,
+  scene:     [MainScene]
 
-let game = new Phaser.Game(config);
+};
+const game = new Phaser.Game(config);
