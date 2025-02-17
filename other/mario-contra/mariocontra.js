503a7162495f7d5d17b1e7ba6633542d8189f413
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-02-17 18:01:35 +0100

    MarioContra 2: shooting

--- other/mario-contra/mariocontra.js
+++ other/mario-contra/mariocontra.js
@@ -5,6 +5,9 @@ class MainScene extends Phaser.Scene {
         super('MainScene');
         this.time = 0;
         this.lastTextureChange = 0;
+        this.lastBulletTime = 0;
+        this.bullets = [];
+        this.bulletAngle = 0;
     }
 
     preload() {
@@ -17,6 +20,7 @@ class MainScene extends Phaser.Scene {
         this.load.image('high-hill', 'files/highhill.png');
         this.load.image('low-hill', 'files/lowhill.png');
         this.load.image('castle', 'files/castle.png');
+        this.load.image('bullet', 'files/bullet.png');
         this.currentCommandoTexture = 'commando';
     }
 
@@ -24,6 +28,7 @@ class MainScene extends Phaser.Scene {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.createSpriteGroup();
         this.commando = this.add.sprite(70, this.sys.canvas.height - 150, 'commando');
+
     }
 
     createSpriteGroup() {
@@ -35,6 +40,31 @@ class MainScene extends Phaser.Scene {
         this.moveEnemies();
         this.checkVictory();
         this.updateHeader(time);
+        this.handleShooting(time);
+    }
+
+    handleShooting(time) {
+        if (time - this.lastBulletTime > 500) {
+            this.createBullet();
+            this.lastBulletTime = time;
+        }
+        this.moveBullets();
+    }
+
+    createBullet() {
+        const bullet = this.add.sprite(this.commando.x + 20, this.commando.y - 19, 'bullet');
+        this.bullets.push(bullet);
+    }
+
+    moveBullets() {
+        for (let i = this.bullets.length - 1; i >= 0; i--) {
+            const bullet = this.bullets[i];
+            bullet.x += 10;
+            if (bullet.x > 800) {
+                bullet.destroy();
+                this.bullets.splice(i, 1);
+            }
+        }
     }
 
     moveBackground(time){
