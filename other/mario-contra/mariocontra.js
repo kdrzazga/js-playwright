f382510f44fa7d55db81d5814566da2bbab831be
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-02-17 17:09:00 +0100

    MarioContra 2: running

--- other/mario-contra/mariocontra.js
+++ other/mario-contra/mariocontra.js
@@ -1,20 +1,23 @@
 class MainScene extends Phaser.Scene {
     static TILE_WIDTH = 60;
-    static COMMANDO_SPEED = 25;
+    static COMMANDO_SPEED = 5;
     constructor() {
         super('MainScene');
         this.time = 0;
+        this.lastTextureChange = 0;
     }
 
     preload() {
         this.load.image('ground', 'files/sprite.png');
         this.load.image('commando', 'files/commando.png');
+        this.load.image('commando2', 'files/commando2.png');
         this.load.image('gumba', 'files/gumba.png');
         this.load.image('turtle', 'files/turtle.png');
         this.load.image('cloud', 'files/cloud.png');
         this.load.image('high-hill', 'files/highhill.png');
         this.load.image('low-hill', 'files/lowhill.png');
         this.load.image('castle', 'files/castle.png');
+        this.currentCommandoTexture = 'commando';
     }
 
     create() {
@@ -57,18 +60,26 @@ class MainScene extends Phaser.Scene {
     }
 
     update(time, delta) {
-        this.moveBackground();
+        this.moveBackground(time);
         this.moveEnemies();
         this.checkVictory();
         this.updateHeader(time);
     }
 
-    moveBackground(){
+    moveBackground(time){
         this.cameras.main.setBackgroundColor(0x507fff);
         if (this.cursors.right.isDown) {
             this.spriteGroup.children.iterate(function (child) {
                 child.x -= MainScene.COMMANDO_SPEED;
             });
+            if (time - this.lastTextureChange > 300) {
+                this.commando.setTexture(this.currentCommandoTexture);
+                this.currentCommandoTexture = (this.currentCommandoTexture === 'commando') ? 'commando2' : 'commando';
+                this.lastTextureChange = time;
+            }
+        }
+        else {
+            this.commando.setTexture('commando');
         }
     }
 
@@ -85,7 +96,7 @@ class MainScene extends Phaser.Scene {
         this.spriteGroup.children.iterate(function (child) {
             if (child.texture.key === 'castle') {
                 if (child.x <= 1*  MainScene.TILE_WIDTH){
-                    window.alert('You win ! The princess is in this very castle.');
+                    window.alert('You win ! The princess is in this particular castle.');
                     location.reload();
                 }
             }
@@ -104,7 +115,17 @@ const config = {
     parent: 'game-container',
     width: 800,
     height: 600,
-    scene: [MainScene]
+    scene: [MainScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 500,
+                x: 0,
+            },
+            debug: false,
+        }
+    }
 };
 
 const game = new Phaser.Game(config);
