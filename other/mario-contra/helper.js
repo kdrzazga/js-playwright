38d8ab3a60e0f87878d9d2bbc34ba8cf18584db8
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-02-21 21:06:52 +0100

    Mario Contra 3: 1 pln

--- other/mario-contra/helper.js
+++ other/mario-contra/helper.js
@@ -17,8 +17,8 @@ class SpriteGroupHelper {
             spriteGroup.add(sprite);
         }
 
-        const floor1BricksX = [9, 19, 21, 23, 25, 78, 79, 80, 95, 96, 97, 98, 99, 100, 101, 102, 110, 111, 119, 128, 129
-            , 140, 141, 143,144, 169, 170, 201, 202, 203, 204, 205, 207, 209, 211, 213, 215];
+        const floor1BricksX = [9, 19, 21, 23, 25, 49,51,53,55, 78, 79, 80, 95, 96, 97, 98, 99, 100, 101, 102, 110, 111
+            , 119, 128, 129, 140, 141, 143,144, 169, 170, 201, 202, 203, 204, 205, 207, 209, 211, 213, 215];
         floor1BricksX.forEach(x => {
             const brick = this.scene.add.sprite(tileWidth * x, floor1Height * tileWidth, 'brick');
             spriteGroup.add(brick);
@@ -30,7 +30,7 @@ class SpriteGroupHelper {
             spriteGroup.add(brick);
         });
 
-        const questionsX = [22, 24, 82, 90, 91, 92, 93, 94, 142, 206, 208, 210, 212, 214];
+        const questionsX = [22, 24, 50,52,54, 82, 90, 91, 92, 93, 94, 142, 206, 208, 210, 212, 214];
         questionsX.forEach(x => {
             const q = this.scene.add.sprite(tileWidth * x, floor1Height * tileWidth, 'question');
             spriteGroup.add(q);
@@ -54,14 +54,16 @@ class SpriteGroupHelper {
         spriteGroup.add(castle);
 
         const yPos = canvasHeight - 105;
-        const gumbas = [15, 17, 41, 51, 53, 109, 120, 131, 133, 135].map(x => this.scene.add.sprite(x * tileWidth, yPos, 'gumba'));
+        const gumbas = [15, 17, 41, 51, 53, 109, 120, 131, 133, 135, 155, 245,266].map(x =>
+                this.scene.add.sprite(x * tileWidth, yPos, 'gumba'));
         gumbas.forEach(gumba => {
             gumba.speedX = 1;
             gumba.speedY = 0;
             spriteGroup.add(gumba);
         });
 
-        const turtles = [90, 102, 140].map(x => this.scene.add.sprite(x * tileWidth, this.scene.sys.canvas.height - 123, 'turtle'));
+        const turtles = [90, 102, 115,116, 137,138,139, 145, 240, 250,260].map(x =>
+                 this.scene.add.sprite(x * tileWidth, this.scene.sys.canvas.height - 123, 'turtle'));
         turtles.forEach(turtle => {
             turtle.speedX = 1;
             turtle.speedY = 0;
