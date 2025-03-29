b444e92464b7c44c8d2efa1a043dfcb378bef2f3
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-03-29 11:10:45 +0100

    intro anim2

--- other/c64intro/intro.js
+++ other/c64intro/intro.js
@@ -1,3 +1,15 @@
+const audio = new Audio('fst.mp3');
+
+function playAudio() {
+    const button = document.getElementById('play');
+    button.style.display = "none";
+    audio.play();
+
+    setTimeout(() => {
+      playAudio();
+    }, 45000);
+  }
+
 class Constants{
     static SCREEN_WIDTH = 800;
     static SCREEN_HEIGHT = 600;
