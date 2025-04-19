349b4660b345c24bac1475fd511994c30d66e175
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-04-19 21:17:26 +0200

    Fight Final 2 (#132)

--- other/fight-final/conf.js
+++ other/fight-final/conf.js
@@ -1,6 +1,7 @@
 const config = {
     type: Phaser.AUTO,
     parent: 'game-container',
+    backgroundColor: '#6699cc',
     width: 1000,
     height: 600,
     scene: [MainScene],
--- other/fight-final/level.js
+++ other/fight-final/level.js
@@ -1,6 +1,7 @@
 class MainScene extends Phaser.Scene {
     static TILE_WIDTH = 60;
     static PLAYER_SPEED = 5;
+    static CURRENT_ID = 1;
 
     constructor(name) {
         super(name);
@@ -19,6 +20,10 @@ class MainScene extends Phaser.Scene {
 	
 	preload(){
 		this.load.image('floor', 'floor.png');
+		this.load.image('door-wall', 'door-wall.png');
+		this.load.image('window-wall', 'window-wall.png');
+		this.load.image('building-end', 'building-end.png');
+
 		this.load.image('d1', 'd1.png');
 		this.load.image('d2', 'd2.png');
 		this.load.image('d3', 'd3.png');
@@ -30,13 +35,27 @@ class MainScene extends Phaser.Scene {
 		this.load.image('dp3', 'dp3.png');
 		this.load.image('dp4', 'dp4.png');
 
+		this.load.image('cw1', 'codyw1.png');
+		this.load.image('cw2', 'codyw2.png');
+		this.load.image('cw3', 'codyw3.png');
+		this.load.image('cw4', 'codyw4.png');
+		this.load.image('cw5', 'codyw5.png');
+
+		this.load.image('hw1', 'haggard1.png');
+		this.load.image('hw2', 'haggard2.png');
+		this.load.image('hw3', 'haggard3.png');
+		this.load.image('hw4', 'haggard4.png');
+		this.load.image('hw5', 'haggard5.png');
+
+		this.load.image('oriber', 'ffOriber.png');
+
 		this.load.audio('laugh', 'hehe.m4a');
 	}
 	
 	create(){
         this.cursors = this.input.keyboard.createCursorKeys();
 		this.damnd = this.add.sprite(this.sys.canvas.height/2, this.sys.canvas.height - 150, 'd1');
-		
+
 		this.anims.create({
             key: 'damnd-stand',
             frames: [
@@ -109,6 +128,35 @@ class MainScene extends Phaser.Scene {
             repeat: 1
         });
 
+        this.anims.create({
+            key: 'cody-walk',
+            frames: [
+                { key: 'cw1'},
+                { key: 'cw2'},
+                { key: 'cw3'},
+                { key: 'cw4'},
+                { key: 'cw5'},
+                { key: 'cw4'},
+                { key: 'cw3'},
+                { key: 'cw2'},
+            ],
+            frameRate: 4,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'haggard-walk',
+            frames: [
+                { key: 'hw1'},
+                { key: 'hw2'},
+                { key: 'hw3'},
+                { key: 'hw4'},
+                { key: 'hw5'}
+            ],
+            frameRate: 5,
+            repeat: -1
+        });
+
         this.laughSound = this.sound.add('laugh');
 
 		this.animKey = 'damnd-stand';
@@ -120,6 +168,16 @@ class MainScene extends Phaser.Scene {
 	    this.move(time);
 	    this.damndCounter += 1;
 	    this.laughConditionally();
+	    this.spriteGroup.children.iterate(sprite => {
+	        if (sprite.data)
+	        if (sprite.data.startsWith('enemy')){
+	                if (sprite.data.endsWith('alive')) sprite.x -= MainScene.PLAYER_SPEED + 2*Math.cos(sprite.id);
+	                if (sprite.y > this.damnd.y)
+	                    sprite.setDepth(1); //this.damnd who is human player has depth = 0
+	                else
+	                    sprite.setDepth(-1);
+	            }
+	    });
 	}
 
 	createSpriteGroup(){
@@ -132,6 +190,48 @@ class MainScene extends Phaser.Scene {
             const sprite = this.add.sprite(x, this.sys.canvas.height - 73, 'floor');
             sprite.setDepth(-5);
             this.spriteGroup.add(sprite);
+        }
+
+        for (let shift = 0; shift <= tileWidth * 22 * 8; shift += tileWidth * 22){
+
+	        for (let i = 5; i < 8; i++) {
+                const x = i * (tileWidth + 22) + shift;
+                const sprite = this.add.sprite(x, this.sys.canvas.height - 385 + 5, 'window-wall');
+                sprite.setDepth(-5);
+                this.spriteGroup.add(sprite);
+            }
+
+            let sprite = this.add.sprite(8.5 * (tileWidth + 11) + shift, this.sys.canvas.height - 385 + 5, 'building-end');
+            sprite.setDepth(-5);
+            this.spriteGroup.add(sprite);
+        }
+
+        for (let i = 0; i < 75; i++){
+		    const cody = this.add.sprite(this.sys.canvas.height * 5 + (0.8 + 0.8*Math.random())*i * this.sys.canvas.width, this.sys.canvas.height - 150 - 60*Math.random(), 'cw1');
+		    cody.play('cody-walk');
+		    cody.data = 'enemy-alive';
+		    cody.id = MainScene.CURRENT_ID;
+		    MainScene.CURRENT_ID += 1;
+		    this.spriteGroup.add(cody);
+		}
+
+        for (let i = 0; i < 62; i++){
+		    const haggard = this.add.sprite(11111 + (0.8 + 0.8*Math.random())*i * this.sys.canvas.width, this.sys.canvas.height - 150 - 60*Math.random(), 'hw1');
+		    haggard.play('haggard-walk');
+		    haggard.data = 'enemy-alive';
+		    haggard.id = MainScene.CURRENT_ID;
+		    MainScene.CURRENT_ID += 1;
+		    haggard.setFlipX(true);
+		    this.spriteGroup.add(haggard);
+		}
+
+        for (let i = 1; i< 24; i += 1){
+		    const deadOriber = this.add.sprite(1000 + i*2222 + 1000*Math.random(), this.sys.canvas.height - 150- 60*Math.random(), 'oriber');
+		    deadOriber.data = 'enemy-dead';
+            deadOriber.setDepth(-4);
+            if (i % 2 < 1)
+                deadOriber.setFlipX(true);
+            this.spriteGroup.add(deadOriber);
         }
 	}
 
