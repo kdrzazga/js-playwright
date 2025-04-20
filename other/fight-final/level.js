90c732af1dc247a6f4ac69a8366c8e9186d32327
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-04-20 17:44:25 +0200

    ff3: refactor

--- other/fight-final/level.js
+++ other/fight-final/level.js
@@ -16,146 +16,18 @@ class MainScene extends Phaser.Scene {
         this.backgroundColor = 'black';
         this.spriteGroup = null;
         this.damndCounter = 0;
+        this.manager = new Manager(this);
     }
 	
-	preload(){
-		this.load.image('floor', 'floor.png');
-		this.load.image('door-wall', 'door-wall.png');
-		this.load.image('window-wall', 'window-wall.png');
-		this.load.image('building-end', 'building-end.png');
-
-		this.load.image('d1', 'd1.png');
-		this.load.image('d2', 'd2.png');
-		this.load.image('d3', 'd3.png');
-		this.load.image('dw1', 'dw1.png');
-		this.load.image('dw2', 'dw2.png');
-		this.load.image('dw3', 'dw3.png');
-		this.load.image('dp1', 'dp1.png');
-		this.load.image('dp2', 'dp2.png');
-		this.load.image('dp3', 'dp3.png');
-		this.load.image('dp4', 'dp4.png');
-
-		this.load.image('cw1', 'codyw1.png');
-		this.load.image('cw2', 'codyw2.png');
-		this.load.image('cw3', 'codyw3.png');
-		this.load.image('cw4', 'codyw4.png');
-		this.load.image('cw5', 'codyw5.png');
-
-		this.load.image('hw1', 'haggard1.png');
-		this.load.image('hw2', 'haggard2.png');
-		this.load.image('hw3', 'haggard3.png');
-		this.load.image('hw4', 'haggard4.png');
-		this.load.image('hw5', 'haggard5.png');
-
-		this.load.image('oriber', 'ffOriber.png');
-
-		this.load.audio('laugh', 'hehe.m4a');
+	preload() {
+		this.manager.preload();
 	}
 	
 	create(){
         this.cursors = this.input.keyboard.createCursorKeys();
 		this.damnd = this.add.sprite(this.sys.canvas.height/2, this.sys.canvas.height - 150, 'd1');
 
-		this.anims.create({
-            key: 'damnd-stand',
-            frames: [
-                { key: 'd1' },
-                { key: 'd2' },
-                { key: 'd2' },
-                { key: 'd3' },
-                { key: 'd2' },
-                { key: 'd3' },
-                { key: 'd2' },
-                { key: 'd3' },
-                { key: 'd1' },
-                { key: 'd2' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd2' },
-                { key: 'd2' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' },
-                { key: 'd1' }
-            ],
-            frameRate: 3,
-            repeat: -1
-        });
-
-		this.anims.create({
-            key: 'damnd-walk',
-            frames: [
-                { key: 'dw1' },
-                { key: 'dw2' },
-                { key: 'dw3' }
-            ],
-            frameRate: 7,
-            repeat: -1
-        });
-
-		this.anims.create({
-            key: 'damnd-punch',
-            frames: [
-                { key: 'dp1' },
-                { key: 'dp2' },
-                { key: 'dp3' },
-                { key: 'dp4' },
-                { key: 'dp4' },
-                { key: 'dp3' },
-                { key: 'dp2' },
-                { key: 'dp1' }
-            ],
-            frameRate: 5,
-            repeat: 1
-        });
-
-        this.anims.create({
-            key: 'cody-walk',
-            frames: [
-                { key: 'cw1'},
-                { key: 'cw2'},
-                { key: 'cw3'},
-                { key: 'cw4'},
-                { key: 'cw5'},
-                { key: 'cw4'},
-                { key: 'cw3'},
-                { key: 'cw2'},
-            ],
-            frameRate: 4,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'haggard-walk',
-            frames: [
-                { key: 'hw1'},
-                { key: 'hw2'},
-                { key: 'hw3'},
-                { key: 'hw4'},
-                { key: 'hw5'}
-            ],
-            frameRate: 5,
-            repeat: -1
-        });
+        this.manager.createAnims();
 
         this.laughSound = this.sound.add('laugh');
 
