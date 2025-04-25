124d6ef6a835dc107e787da845ebe1f52b02b523
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-04-25 08:21:00 +0200

    Maldenbrot class

--- other/fractals/mandelbrot.js
+++ other/fractals/mandelbrot.js
@@ -1,61 +1,79 @@
 const maxWidth = 640;
 const maxHeight = 640;
 
+class MandelbrotScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MandelbrotScene' });
+        this.xMin = -2.0;
+        this.xMax = 1.0;
+        this.yMin = -1.0;
+        this.yMax = 1.0;
+        this.width = maxWidth;
+        this.height = maxHeight;
+        this.maxIter = 100;
+    }
+
+    preload() {
+    }
+
+    create() {
+        this.graphics = this.add.graphics();
+        this.drawMandelbrot();
+    }
+
+    getMandelParamsFromUi() {
+        const iterationsSlider = document.getElementById('iterationSliderM');
+        const iterations = parseInt(iterationsSlider.value, 10);
+        this.maxIter = iterations;
+        this.drawMandelbrot();
+    }
+
+    drawMandelbrot() {
+        const pixelWidth = 1;
+        this.graphics.clear();
+
+        for (let px = 0; px < this.width; px++) {
+            for (let py = 0; py < this.height; py++) {
+                let x0 = this.xMin + (px / this.width) * (this.xMax - this.xMin);
+                let y0 = this.yMin + (py / this.height) * (this.yMax - this.yMin);
+                let x = 0, y = 0;
+                let iteration = 0;
+
+                while (x * x + y * y <= 4 && iteration < this.maxIter) {
+                    let xtemp = x * x - y * y + x0;
+                    y = 2 * x * y + y0;
+                    x = xtemp;
+                    iteration++;
+                }
+
+                const colorValue = (iteration === this.maxIter) ? 0 : (iteration / this.maxIter) * 255;
+                const color = Phaser.Display.Color.GetColor(colorValue, colorValue, 0);
+
+                this.graphics.fillStyle(color, 1);
+                this.graphics.fillRect(px * pixelWidth, py * pixelWidth, pixelWidth, pixelWidth);
+            }
+        }
+    }
+
+    update() {
+    }
+}
+
+function getMandelParamsFromUi() {
+    const scene = game.scene.getScene('MandelbrotScene');
+    if (scene) {
+        scene.getMandelParamsFromUi();
+    }
+}
+
 const config = {
     type: Phaser.AUTO,
     parent: 'mandelbrot',
     width: maxWidth,
     height: maxHeight,
     backgroundColor: '#000000',
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: MandelbrotScene
 };
 
 const game = new Phaser.Game(config);
-let graphics = null;
 
-function preload() {
-}
-
-function create() {
-    graphics = this.add.graphics();
-    drawMandelbrot(graphics, -2.0, 1.0, 1.0, -1.0, maxWidth, maxHeight, 100);
-}
-
-function getParamsFromUi(){
-    const iterationsSlider = document.getElementById('iterationSliderM');
-    const iterations = iterationsSlider.value;
-    drawMandelbrot(graphics, -2.0, 1.0, 1.0, -1.0,  maxWidth, maxHeight, iterations);
-}
-
-function update() {
-}
-
-function drawMandelbrot(graphics, xMin, xMax, yMin, yMax, width, height, maxIter) {
-    const pixelWidth = 1;
-
-    for (let px = 0; px < width; px++) {
-        for (let py = 0; py < height; py++) {
-            let x0 = xMin + (px / width) * (xMax - xMin);
-            let y0 = yMin + (py / height) * (yMax - yMin);
-            let x = 0, y = 0;
-            let iteration = 0;
-
-            while (x * x + y * y <= 4 && iteration < maxIter) {
-                let xtemp = x * x - y * y + x0;
-                y = 2 * x * y + y0;
-                x = xtemp;
-                iteration++;
-            }
-
-            const colorValue = (iteration === maxIter) ? 0 : (iteration / maxIter) * 255;
-            const color = Phaser.Display.Color.GetColor(colorValue, colorValue, 0); // Gradient from black to green
-
-            graphics.fillStyle(color, 1);
-            graphics.fillRect(px * pixelWidth, py * pixelWidth, pixelWidth, pixelWidth);
-        }
-    }
-}
