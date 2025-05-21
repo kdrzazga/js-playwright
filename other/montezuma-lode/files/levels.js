de99c42c6f459fe99e94b3fdd749baecda903dcc
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-05-21 16:35:26 +0200

    Mariozuma Lode 2 (#139)

--- other/montezuma-lode/files/levels.js
+++ other/montezuma-lode/files/levels.js
@@ -4,7 +4,37 @@ class Scene1 extends MainScene{
         super('Scene1');
 
         this.nonBrickRows = [1,2];
+        this.skullRows= [ {'row': 2, 'side': 'right'} ];
+        this.nextScene['right'] = 'Scene2';
+        this.exits['right']['x'] = '13';
+        this.exits['right']['y'] = '2';
+    }
+
+    create(){
+        super.create();
+        this.createSpriteGroup();
+    }
+
+    createSpriteGroup() {
+        super.createSpriteGroup();
+    }
+
+}
+
+class Scene2 extends MainScene{
+
+    constructor(){
+        super('Scene2');
+
+        this.nonBrickRows = [1,2];
         this.nonBrickColumns = [5];
+
+        this.skullRows= [ {'row': 2, 'side': 'right'} ];
+        this.ladderColumns = [ {'column' : 5, 'start' : 3, 'end' : 11}];
+
+        this.nextScene['left'] = 'Scene1';
+        this.exits['left']['x'] = '0';
+        this.exits['left']['y'] = '2';
     }
 
     create(){
