2d2e408cae63ef10be0ab3259f60ecee4c2e59fb
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-05-21 19:50:33 +0200

    4 scenes

--- other/montezuma-lode/files/conf.js
+++ other/montezuma-lode/files/conf.js
@@ -3,7 +3,7 @@ const config = {
     parent: 'game-container',
     width: 800,
     height: 600,
-    scene: [Scene1, Scene2],
+    scene: [Scene1, Scene2, Scene3, Scene4],
     physics: {
         default: 'arcade',
         arcade: {
--- other/montezuma-lode/files/levels.js
+++ other/montezuma-lode/files/levels.js
@@ -26,13 +26,70 @@ class Scene2 extends MainScene{
     constructor(){
         super('Scene2');
 
+        this.nonBrickRows = [1,2, 4,6, 8];
+
+        this.skullRows= [ {'row': 4, 'side': 'left'}, {'row': 6, 'side': 'right'} , {'row': 8, 'side': 'left'} ];
+
+        this.nextScene['left'] = 'Scene1';
+        this.nextScene['right'] = 'Scene3';
+        this.exits['left']['x'] = '0';
+        this.exits['left']['y'] = '2';
+        this.exits['right']['x'] = '13';
+        this.exits['right']['y'] = '2';
+    }
+
+    create(){
+        super.create();
+        this.createSpriteGroup();
+    }
+
+    createSpriteGroup() {
+        super.createSpriteGroup();
+    }
+}
+
+class Scene3 extends MainScene{
+
+    constructor(){
+        super('Scene3');
+        this.backgroundColor = 'black';
+
+        this.nonBrickRows = [0,1,2,3,4,5,6,7,8,9,10];
+
+        this.skullRows= [ {'row': 4, 'side': 'left'}, {'row': 6, 'side': 'right'} , {'row': 8, 'side': 'left'} ];
+
+        this.nextScene['left'] = 'Scene2';
+        this.nextScene['right'] = 'Scene4';
+        this.exits['left']['x'] = '0';
+        this.exits['left']['y'] = '2';
+        this.exits['right']['x'] = '13';
+        this.exits['right']['y'] = '2';
+    }
+
+    create(){
+        super.create();
+        this.createSpriteGroup();
+    }
+
+    createSpriteGroup() {
+        super.createSpriteGroup();
+    }
+}
+
+
+class Scene4 extends MainScene{
+
+    constructor(){
+        super('Scene4');
+
         this.nonBrickRows = [1,2];
         this.nonBrickColumns = [5];
 
         this.skullRows= [ {'row': 2, 'side': 'right'} ];
         this.ladderColumns = [ {'column' : 5, 'start' : 3, 'end' : 11}];
 
-        this.nextScene['left'] = 'Scene1';
+
+        this.nextScene['left'] = 'Scene3';
         this.exits['left']['x'] = '0';
         this.exits['left']['y'] = '2';
     }
@@ -45,5 +102,4 @@ class Scene2 extends MainScene{
     createSpriteGroup() {
         super.createSpriteGroup();
     }
-
 }
