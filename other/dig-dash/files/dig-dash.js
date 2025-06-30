930a499ed385bc7145ffcc0e35685a30050b290c
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-06-30 12:46:39 +0200

    further move

--- other/dig-dash/files/dig-dash.js
+++ other/dig-dash/files/dig-dash.js
@@ -4,15 +4,22 @@ class MyScene extends Phaser.Scene {
 
     constructor() {
         super({ key: 'MyScene' });
+        this.meshShiftX = 0;
+        this.meshShiftY = 0;
     }
 
     preload() {
         this.load.image('player', 'files/dd.bmp');
+        this.load.image('dirt-tile', '../common/pics/dirt.png');
     }
 
     create() {
-        this.player = this.physics.add.sprite(150, 150, 'player');
-        this.playerSpeed = 100;
+        this.spriteGroup = this.add.group();
+        this.container = this.add.container(0, 0);
+        this.player = this.physics.add.sprite(7*MyScene.TILE_SIZE, 2*MyScene.TILE_SIZE, 'player');
+        const tile = this.physics.add.sprite(4*MyScene.TILE_SIZE, 3*MyScene.TILE_SIZE, 'dirt-tile');
+        tile.setDepth(-5);
+        this.spriteGroup.add(tile);
 
         this.currentDirection = 'right'; // 'left', 'right', 'up', 'down'
         this.targetPosition = new Phaser.Math.Vector2(this.player.x, this.player.y);
@@ -23,10 +30,11 @@ class MyScene extends Phaser.Scene {
     }
 
     drawMesh(){
+        this.container = this.add.container(0, 0);
         const graphics = this.add.graphics();
         graphics.lineStyle(1, 0x00cc00);
-        const width = this.sys.game.config.width;
-        const height = this.sys.game.config.height;
+        const width = 4*this.sys.game.config.width;
+        const height = 3*this.sys.game.config.height;
 
         for (let x = 0; x <= width; x += MyScene.TILE_SIZE) {
             graphics.moveTo(x, 0);
@@ -37,11 +45,12 @@ class MyScene extends Phaser.Scene {
             graphics.lineTo(width, y);
         }
         graphics.strokePath();
+
+        this.spriteGroup.add(this.container);
+        this.container.add(graphics);
     }
 
     update(time, delta) {
-        const maxX = Math.floor(this.sys.game.config.width / MyScene.TILE_SIZE) * MyScene.TILE_SIZE;
-        const maxY = Math.floor(this.sys.game.config.height / MyScene.TILE_SIZE) * MyScene.TILE_SIZE;
 
         if (this.cursors.left.isDown) {
             if (this.isAlignedY()) this.currentDirection = 'left';
@@ -55,18 +64,57 @@ class MyScene extends Phaser.Scene {
 
         if (this.currentDirection) {
             switch(this.currentDirection){
-                case 'left': if(this.player.x > MyScene.TILE_SIZE) this.player.x--; break;
-                case 'right': if(this.player.x < maxX)  this.player.x++; break;
-                case 'up': if(this.player.y > MyScene.TILE_SIZE)  this.player.y--; break;
-                case 'down': if(this.player.y < maxY)  this.player.y++; break;
+                case 'left': this.moveLeft(); break;
+                case 'right': this.moveRight(); break;
+                case 'up': this.moveUp(); break;
+                case 'down': this.moveDown(); break;
             }
         }
 
     this.currentDirection = '';
     }
 
+    moveLeft(){
+        const movement = -1;
+        const limit = MyScene.TILE_SIZE;
+        const maxX = Math.floor(this.sys.game.config.width / MyScene.TILE_SIZE) * MyScene.TILE_SIZE;
+        if(this.player.x > limit) this.player.x += movement;
+        else {
+            this.spriteGroup.children.iterate( s => s.x++);
+            //this.meshShiftX = (this.meshShiftX - movement) % MyScene.TILE_SIZE;
+        }
+        if(this.player.x > limit) this.player.x += movement;
+    }
+
+    moveRight(){
+        const maxX = Math.floor(this.sys.game.config.width / MyScene.TILE_SIZE) * MyScene.TILE_SIZE;
+        const limit = 3/4*maxX;
+        if (this.player.x < limit)  this.player.x++;
+        else {
+            this.spriteGroup.children.iterate( s => s.x--);
+            //this.meshShiftX = (this.meshShiftX - 1) % MyScene.TILE_SIZE;
+        }
+    }
+
+    moveUp(){
+        if(this.player.y > MyScene.TILE_SIZE)  this.player.y--;
+        else {
+            this.spriteGroup.children.iterate( s => s.y++);
+            //this.meshShiftY = (this.meshShiftY + 1) % MyScene.TILE_SIZE;
+        }
+    }
+
+    moveDown(){
+        const maxY = Math.floor(this.sys.game.config.height / MyScene.TILE_SIZE) * MyScene.TILE_SIZE;
+        if(this.player.y < maxY)  this.player.y++;
+        else {
+            this.spriteGroup.children.iterate( s => s.y--);
+            //this.meshShiftY = (this.meshShiftY - 1) % MyScene.TILE_SIZE;
+        }
+    }
+
     isAlignedX(){
-        if (this.player.x % MyScene.TILE_SIZE < MyScene.TILE_SIZE /3){
+        if ((this.player.x + this.meshShiftX)  % MyScene.TILE_SIZE < MyScene.TILE_SIZE /3){
             const newX = Math.floor(this.player.x / MyScene.TILE_SIZE) * MyScene.TILE_SIZE;
             this.player.x = newX;
             return true;
@@ -75,7 +123,7 @@ class MyScene extends Phaser.Scene {
     }
 
     isAlignedY(){
-        if (this.player.y % MyScene.TILE_SIZE < MyScene.TILE_SIZE /3){
+        if ((this.player.y + this.meshShiftY) % MyScene.TILE_SIZE < MyScene.TILE_SIZE /3){
             const newY = Math.floor(this.player.y / MyScene.TILE_SIZE) * MyScene.TILE_SIZE;
             this.player.y = newY;
             return true;
